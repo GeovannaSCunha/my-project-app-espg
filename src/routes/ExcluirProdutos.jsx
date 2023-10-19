34a1b2b33f4ddd38d160c60ab09da069ce715fe4
GeovannaSCunha <geovanna.scunha@gmail.com>
2023-10-19 18:21:06 -0300

    Arrumando as paginas de editar e excluir

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -9,3 +9,43 @@ export default function ExcluirProdutos() {
     </div>
   )
 }
+import  { useState, useEffect } from "react";
+import {  useParams, useNavigate } from "react-router-dom";
+import './Excluir.scss'
+export default function ExcluirProdutos() {
+  document.title = "Excluir Produto";
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+  const [produto, setProduto] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/produtos/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProduto(data))
+      .catch((error) => console.log(error));
+  }, [id]);
+
+  const handleExcluir = () => {
+    fetch(`http://localhost:5000/produtos/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        alert("Produto Exluido com sucesso")
+        navigate("/produtos");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  if (!produto) {
+    return <div>Carregando...</div>;
+  }
+
+  return (
+    <div>
+      <h2>Confirmação de Exclusão</h2>
+      <p className="subtitle">Deseja realmente excluir o produto: <span>{produto.nome}?</span></p>
+      <button onClick={handleExcluir}>Confirmar Exclusão</button>
+    </div>
+  );
+}
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,40 +1,15 @@
-import  { useState, useEffect } from "react";
-import {  useParams, useNavigate } from "react-router-dom";
-import './Excluir.scss'
-export default function ExcluirProdutos() {
-  document.title = "Excluir Produto";
-  const navigate = useNavigate();
 
-  const { id } = useParams();
-  const [produto, setProduto] = useState(null);
-
-  useEffect(() => {
-    fetch(`http://localhost:5000/produtos/${id}`)
-      .then((response) => response.json())
-      .then((data) => setProduto(data))
-      .catch((error) => console.log(error));
-  }, [id]);
-
-  const handleExcluir = () => {
-    fetch(`http://localhost:5000/produtos/${id}`, {
-      method: "DELETE",
-    })
-      .then(() => {
-        alert("Produto Exluido com sucesso")
-        navigate("/produtos");
-      })
-      .catch((error) => console.log(error));
-  };
-
-  if (!produto) {
-    return <div>Carregando...</div>;
-  }
-
+export default function Home() {
   return (
     <div>
-      <h2>Confirmação de Exclusão</h2>
-      <p className="subtitle">Deseja realmente excluir o produto: <span>{produto.nome}?</span></p>
-      <button onClick={handleExcluir}>Confirmar Exclusão</button>
+      <h1>Home</h1>
+        <div>
+          <h2>Produtos em Oferta...</h2>
+          <figure>
+            <img src="/img/supermarket-shelf-1094812_640.png" alt="Prateleira" />
+            <figcaption>Nosso produtos</figcaption>
+          </figure>
+        </div>
     </div>
-  );
+  )
 }
