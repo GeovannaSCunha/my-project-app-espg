74f2546a88e3dcb4acf876c649ee7db1f338df6f
MirellaRossi <rm97990@fiap.com.br>
2023-10-22 11:26:51 -0300

    Alteração na fraseologia

--- src/components/ModalAction/ModalExcluir.jsx
+++ src/components/ModalAction/ModalExcluir.jsx
@@ -18,9 +18,10 @@ export default function ModalExcluir(props) {
     fetch(`http://localhost:5000/produtos/${id}`, {
       method: "DELETE",
     })
+    //Após a confirmação da exclusão, exibe o alerta e fecha o modal, retornando aos produtos
       .then(() => {
         alert("Produto Excluído com sucesso");
-        props.onClose();
+        props.onClose(); //Fecha o modal
         navigate("/produtos");
       })
       .catch((error) => console.log(error));
@@ -29,18 +30,18 @@ export default function ModalExcluir(props) {
   if (!produto) {
     return <div>Carregando...</div>;
   }
-
+// Criando o formulário de exclusão do produto
   return (
     <div className="custom-modal">
       <div className="modal-content">
-        <h2>Confirmação de Exclusão</h2>
+        <h2>Exclusão</h2>
         <p className="subtitle">
           Deseja realmente excluir o produto: "<span className="produto">{produto.nome}</span>"?
         </p>
         <h3>Informações do Produto:</h3>
         <ul>
-          <li><strong>Descrição:</strong> {produto.desc}</li>
-          <li><strong>Preço:</strong> R${produto.preco}</li>
+          <li><strong>Descrição: </strong> {produto.desc}</li>
+          <li><strong>Preço: </strong> R${produto.preco}</li>
         </ul>
         <button onClick={handleExcluir}>Confirmar Exclusão</button>
         <button onClick={props.onClose}>Cancelar</button>
