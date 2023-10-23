09226944cc3a290f80eaf0bec4838382cee8d1e1
GeovannaSCunha <geovanna.scunha@gmail.com>
2023-10-23 18:55:43 -0300

    Add comentários

--- src/components/ModalAction/ModalAction.jsx
+++ src/components/ModalAction/ModalAction.jsx
@@ -8,6 +8,7 @@ export default function ModalAction(props) {
   const { id } = useParams();
   const [produto, setProduto] = useState(null);
 
+  // UseEffect para buscar informações do produto quando o modal é aberto ou o ID muda
   useEffect(() => {
     if (open) {
       fetch(`http://localhost:5000/produtos/${id}`)
@@ -17,22 +18,26 @@ export default function ModalAction(props) {
     }
   }, [id, open]);
 
+  // Função para lidar com a exclusão do produto
   const handleExcluir = () => {
     fetch(`http://localhost:5000/produtos/${id}`, {
       method: "DELETE",
     })
       .then(() => {
-        alert("Produto Exluido com sucesso");
+        // Após a exclusão bem-sucedida, exibe um alerta, fecha o modal e navega de volta para a página de produtos
+        alert("Produto Excluído com sucesso");
         onClose(); // Fecha o modal
-        navigate("/produtos");
+        navigate("/produtos"); // Navega para a página de produtos
       })
       .catch((error) => console.log(error));
   };
 
+  // Verifica se o modal não está aberto ou os dados do produto não estão disponíveis
   if (!open || !produto) {
     return null; // Não renderizar nada se o modal não estiver aberto ou os dados do produto não estiverem prontos
   }
 
+  // Renderização do formulário de confirmação de exclusão
   return (
     <div className="custom-modal">
       <div className="modal-content">
--- src/routes/AdicionarProdutos.jsx
+++ src/routes/AdicionarProdutos.jsx
@@ -1,97 +1,105 @@
-
-import { useNavigate, useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import './AdicionarProdutos.scss';
 
 export default function AdicionarProduto() {
+  // Define o título da página
+  document.title = "ADICIONAR PRODUTO";
 
-    //Recuperando o parâmetro ID com o HOOK useParams();
-    document.title = "EDITAR PRODUTOS";
-    const navigate = useNavigate();
-    const [novoProduto, setNovoProduto] = useState({
-        id: '', 
-        nome: '',
-        desc: '',
-        preco: ''
-      });
-     
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setNovoProduto({ ...novoProduto, [name]: value });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        fetch('http://localhost:5000/produtos', {
-          method: 'POST',
-          body: JSON.stringify(novoProduto),
-          headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log('Produto adicionado com sucesso:', data);
-            navigate("/produtos");
-          })
-          .catch((error) => console.error('Erro ao adicionar o produto:', error));
+  // Obtém uma função de navegação para redirecionar após a adição do produto
+  const navigate = useNavigate();
 
-      };
+  // Cria um estado para armazenar informações sobre o novo produto
+  const [novoProduto, setNovoProduto] = useState({
+    id: '', 
+    nome: '',
+    desc: '',
+    preco: ''
+  });
+ 
+  // Função chamada quando um campo do formulário é alterado
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // Atualiza o estado com os valores do campo alterado
+    setNovoProduto({ ...novoProduto, [name]: value });
+  };
 
+  // Função chamada quando o formulário é submetido
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-      return (
+    // Envia uma requisição POST para adicionar o novo produto
+    fetch('http://localhost:5000/produtos', {
+      method: 'POST',
+      body: JSON.stringify(novoProduto),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Após o sucesso, exibe uma mensagem no console e redireciona para a página de produtos
+        console.log('Produto adicionado com sucesso:', data);
+        navigate("/produtos");
+      })
+      .catch((error) => console.error('Erro ao adicionar o produto:', error));
+  };
+
+  // Renderização do componente
+  return (
+    <div className="addProduto">
+      <h1>ADICIONAR PRODUTO</h1>
+      <form onSubmit={handleSubmit}>
+        {/* Campos do formulário */}
         <div className="addProduto">
-          <h1>ADICIONAR PRODUTO</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="addProduto">
-              <label htmlFor="id">ID</label>
-              <input
-                type="text"
-                name="id"
-                id="id"
-                placeholder="Digite o ID do produto"
-                value={novoProduto.id}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="addProduto">
-              <label htmlFor="nome">Nome</label>
-              <input
-                type="text"
-                name="nome"
-                id="nome"
-                placeholder="Digite o nome do produto"
-                value={novoProduto.nome}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="addProduto">
-              <label htmlFor="desc">Descrição</label>
-              <input
-                type="text"
-                name="desc"
-                id="desc"
-                placeholder="Digite a descrição do produto"
-                value={novoProduto.desc}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="addProduto">
-              <label htmlFor="preco">Preço</label>
-              <input
-                type="text"
-                name="preco"
-                id="preco"
-                placeholder="Digite o preço do produto"
-                value={novoProduto.preco}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <button type="submit">ADICIONAR</button>
-            </div>
-          </form>
+          <label htmlFor="id">ID</label>
+          <input
+            type="text"
+            name="id"
+            id="id"
+            placeholder="Digite o ID do produto"
+            value={novoProduto.id}
+            onChange={handleChange}
+          />
         </div>
-      );
-    }
+        <div className="addProduto">
+          <label htmlFor="nome">Nome</label>
+          <input
+            type="text"
+            name="nome"
+            id="nome"
+            placeholder="Digite o nome do produto"
+            value={novoProduto.nome}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProduto">
+          <label htmlFor="desc">Descrição</label>
+          <input
+            type="text"
+            name="desc"
+            id="desc"
+            placeholder="Digite a descrição do produto"
+            value={novoProduto.desc}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProduto">
+          <label htmlFor="preco">Preço</label>
+          <input
+            type="text"
+            name="preco"
+            id="preco"
+            placeholder="Digite o preço do produto"
+            value={novoProduto.preco}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Botão para submeter o formulário */}
+        <div>
+          <button type="submit">ADICIONAR</button>
+        </div>
+      </form>
+    </div>
+  );
+}
